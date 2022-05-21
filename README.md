83b814e9fc3e185cc20731f68d097ce43cc800bc
Tye Stanley <tyestanley090@gmail.com>
2022-05-20 19:26:20 -0500

    added questions for readme

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Professional README Generator
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+![License Badge](https://img.shields.io/github/license/TyeStanley/readme-generator)
+
+A command-line tool that creates a professional README.md file based on the user's input. Allowing the project's creator to focus more time on the project.
+
+## Installation
+
+1. Download/clone the repository
+2. Must use Node.js to run the tool
+3. `npm install` to install the required dependencies
+
+## Usage
+
+
+
+## Features
+
+ * JavaScript
+ * Node.js
+ * NPM
+
+ ## License
+
+  Copyright (c) Tye Stanley. All rights reserved.
+  
+  Licensed under the [MIT](LICENSE) License.
--- index.js
+++ index.js
@@ -1,13 +1,106 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const emailValidator = require('email-validator');
+
+const generateMarkdown = require('generate-markdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What do you want your project title to be?',
+        validate: (value) => {
+            if (value) { return true } else { return 'Please enter your project title.' }
+        }
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'What is the repository name of your project?',
+        validate: (value) => {
+            if (value) { return true } else { return 'Please enter your repository name.' }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What do you want your project description to be?',
+        validate: (description) => {
+            if (value) { return true } else { return 'Please enter description.' }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter your instructions for installation',
+        validate: (value) => {
+            if (value) { return true } else { return 'Please enter your installation instructions.' }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information?',
+        validate: (value) => {
+            if (value) { return true } else { return 'Please enter usage information.' }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines?',
+        validate: (value) => {
+            if (value) { return true } else { return 'Please enter your contribution guidelines.' }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What are the test instructions?',
+        validate: (value) => {
+            if (value) { return true } else { return 'Please enter your test instructions.' }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        choices: ['MIT', 'ISC', 'GNU GPLv3'],
+        validate: (value) => {
+            if (value) { return true } else { return 'Please select a license.' }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: (value) => {
+            if (value) { return true } else { return 'Please enter your username.' }
+        },
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email addres?',
+        validate: (value) => {
+            if (emailValidator.validate(value)) {
+                return true
+            } else { return 'Please enter a valid email address' }
+        }
+    }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
